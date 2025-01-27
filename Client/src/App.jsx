c443f16ea04ff77672c48f75fd75a977db509b01
Sagar~ <sagargope2001@gmail.com>
2025-01-27 19:51:39 +0530

    chores: [client] set up React with Vite and integrate Tailwind CSS for styling

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="h-screen flex flex-col justify-center items-center bg-gray-950 text-gray-300">
+                Hellow !
+            </div>
+        </>
+    );
+}
+
+export default App;
